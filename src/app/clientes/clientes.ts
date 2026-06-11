5599a4dd1d970d017121d42c38604fee2e5d47ab
Thomas <thomysosa8@gmail.com>
2026-06-11 19:03:11 -0300

    Cuarto commit

--- src/app/clientes/clientes.ts
+++ src/app/clientes/clientes.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clientes',
   imports: [],
   templateUrl: './clientes.html',
-  styleUrl: './clientes.css',
+  styleUrl: './clientes.css'
 })
-export class Clientes {}
+export class Clientes implements OnInit{
+ public clients:any=[];
+  ngOnInit(): void {
+    this.clients= [{Nombre: "Juan",Apel:"Rodriguez",Activo:true},
+{Nombre: "Jose",Apel:"Perez",Activo:true},
+{Nombre: "Pedro",Apel:"Gutierrez",Activo:false},
+{Nombre: "Maria",Apel:"Tossi",Activo:true},
+{Nombre: "Juana",Apel:"Fernandez",Activo:true},
+{Nombre: "Marcelo",Apel:"Diaz",Activo:false},
+{Nombre: "Kevin",Apel:"Motomel",Activo:true},
+{Nombre: "Ricardo",Apel:"Tercero",Activo:true},
+{Nombre: "Juliana",Apel:"Awada",Activo:false}
+    
+  ];  
+
+  }
+
+
+}
--- src/app/sucursales/sucursales.ts
+++ src/app/sucursales/sucursales.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sucursales',
   imports: [],
   templateUrl: './sucursales.html',
-  styleUrl: './sucursales.css',
+  styleUrl: './sucursales.css'
 })
-export class Sucursales {}
+export class Sucursales implements OnInit{
+
+  suc:any = new Array(5);
+  
+  ngOnInit(): void {
+  this.suc= [{id: "Santa Fe",nEmp:20,dir: "San Martin 1111"},
+    {id: "Rosario",nEmp:15,dir: "Rioja 2222"},
+    {id: "Paraná",nEmp:10,dir: "San Jerónimo 3333"},
+    {id: "Chajarí",nEmp:30,dir: "9 de julio 4444"},
+    {id: "Concordia",nEmp:5,dir: "Costanera 5555"}
+  ];  
+  }
+
+  
+
+}
