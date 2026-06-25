82e080481360646a1a747b237855ad147099f342
jcavanna <jcavanna14@gmail.com>
2026-06-25 19:32:41 -0300

    Reloj Funcional

--- src/app/reloj/reloj.ts
+++ src/app/reloj/reloj.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-reloj',
@@ -9,13 +9,16 @@ import { Component } from '@angular/core';
 export class Reloj {
   horaActual: string = '';
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
     this.actualizarHora();
-    setInterval(() => this.actualizarHora(), 1000);
+    setInterval(() => {
+      this.actualizarHora();
+      this.cdr.detectChanges();
+    }, 1000);
   }
 
   actualizarHora() {
     const ahora = new Date();
-    this.horaActual = ahora.toLocaleTimeString();
+    this.horaActual = ahora.toLocaleTimeString('es-AR', { hour12: false });
   }
 }
